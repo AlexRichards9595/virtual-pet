aae4a8058a07b6dd4672e410105a0e0866ee3b7a
Alex <wcadidas15@gmail.com>
2018-01-26 11:51:50 -0500

    added tick and feed, sleep, drink

--- src/main/java/virtualPet/VirtualPet.java
+++ src/main/java/virtualPet/VirtualPet.java
@@ -2,4 +2,57 @@ package virtualPet;
 
 public class VirtualPet {
 
+	private int hunger;
+	private int thirst;
+	private int sleep;
+
+	public int getHunger() {
+		return hunger;
+	}
+
+	public VirtualPet() {
+		hunger = 50;
+		thirst = 50;
+		sleep = 50;
+	}
+
+	public int getThirst() {
+		return thirst;
+	}
+
+	public int getSleep() {
+		return sleep;
+	}
+
+	public int tickHunger(int incomingHunger) {
+		hunger = incomingHunger - 3;
+		return hunger;
+
+	}
+
+	public int tickThirst(int incomingThirst) {
+		thirst = incomingThirst - 7;
+		return thirst;
+	}
+
+	public int tickSleep(int incomingSleep) {
+		sleep = incomingSleep - 10;
+		return sleep;
+	}
+
+	public int feed(int incomingHunger) {
+		hunger = incomingHunger + 5;
+		return hunger;
+	}
+
+	public int drink(int incomingThirst) {
+		thirst = incomingThirst+10;
+		return incomingThirst;
+	}
+
+	public int sleep(int incomingSleep) {
+		sleep = incomingSleep+15;
+		return sleep;
+	}
+
 }
--- src/test/java/virtualPet/VirtualPetTest.java
+++ src/test/java/virtualPet/VirtualPetTest.java
@@ -1,5 +1,108 @@
 package virtualPet;
 
-public class VirtualPetTest {
+import org.junit.Test;
+import org.junit.Assert;
 
+public class VirtualPetTest {
+	
+
+	
+	
+	@Test
+	public void shouldHaveInitialHunger() {
+		// arange
+		VirtualPet underTest = new VirtualPet();
+		// act
+		int hunger = underTest.getHunger();
+		// assert
+		Assert.assertEquals(50, hunger);
+	}
+
+	@Test
+	public void shouldHaveInitialThirst() {
+		// arange
+		VirtualPet underTest = new VirtualPet();
+		// act
+		int thirst = underTest.getThirst();
+		// assert
+		Assert.assertEquals(50, thirst);
+	}
+
+	@Test
+	public void shouldHaveInitialSleep() {
+		// arange
+		VirtualPet underTest = new VirtualPet();
+		// act
+		int sleep = underTest.getSleep();
+		// assert
+		Assert.assertEquals(50, sleep);
+	}
+	@Test
+	public void shouldSubstractHunger() {
+		//arange
+		VirtualPet underTest = new VirtualPet();
+		//act 
+		int hunger = underTest.getHunger();
+		underTest.tickHunger(hunger);
+		int newHunger = underTest.getHunger();
+		//assert
+		Assert.assertEquals(hunger-3, newHunger);
+	}
+	@Test
+	public void shouldSubstractThirst() {
+		//arange
+		VirtualPet underTest = new VirtualPet();
+		//act 
+		int thirst = underTest.getThirst();
+		underTest.tickThirst(thirst);
+		int newThirst = underTest.getThirst();
+		//assert
+		Assert.assertEquals(thirst-7, newThirst);
+	}
+	@Test
+	public void shouldSubstractSleep() {
+		//arange
+		VirtualPet underTest = new VirtualPet();
+		//act 
+		int sleep = underTest.getSleep();
+		underTest.tickSleep(sleep);
+		int newSleep = underTest.getSleep();
+		//assert
+		Assert.assertEquals(sleep-10, newSleep);
+	}
+	
+	@Test
+	public void shouldAddHunger() {
+		//arange
+		VirtualPet underTest = new VirtualPet();
+		//act 
+		int hunger = underTest.getHunger();
+		underTest.feed(hunger);
+		int newHunger = underTest.getHunger();
+		//assert
+		Assert.assertEquals(hunger+5, newHunger);
+	}
+	
+	@Test
+	public void shouldAddThirst() {
+		//arange
+		VirtualPet underTest = new VirtualPet();
+		//act 
+		int thirst = underTest.getThirst();
+		underTest.drink(thirst);
+		int newThirst = underTest.getThirst();
+		//assert
+		Assert.assertEquals(thirst+10, newThirst);
+	}
+	@Test
+	public void shouldAddSleep() {
+		//arange
+		VirtualPet underTest = new VirtualPet();
+		//act 
+		int sleep = underTest.getSleep();
+		underTest.sleep(sleep);
+		int newSleep = underTest.getSleep();
+		//assert
+		Assert.assertEquals(sleep+15, newSleep);
+	}
 }
